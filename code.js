0553b5b1a7dc8e59e476c4e8a7fb9ba812a253ff
wietzeschr <schryvers.wietze@gmail.com>
2024-04-14 11:34:43 +0200

    beta versie nieuwe generator (enkel niveau beginner)

--- code.js
+++ code.js
@@ -30,7 +30,6 @@ class pterm
 
     expand()
     {
-        var res = [this];
         var found = false;
         for (var i = 0; i < this.length && !found; i++)
         {
@@ -69,15 +68,35 @@ class pterm
         {
             if (this.term[i] != pother.term[i] && (this.term[i] != -1 || pother.term[i]  != -1))
             {
-                res += i;
+                res.push(i);
             }
         }
         return res;
     }
 
+    getVarIndex() {
+        var res = [];
+        for (var i = 0; i < this.length; i++) {
+            if (this.term[i] == 0 || this.term[i] == 1) {
+                res[res.length] = i;
+            }
+        }
+        return res;
+    }
+
+    flip(i)
+    {
+        if (this.term[i] == 0) {
+            this.setVar(i, 1);
+        }
+        else if (this.term[i] == 1) {
+            this.setVar(i, 0);
+        }
+    }
+
     toHTML()
     {
-        var  res = ""
+        var  res = "";
         for (var i = 0; i < this.length; i++)
         {
             if (this.term[i] == 1)
@@ -112,17 +131,20 @@ class lexp
     constructor(pterms)
     {
         this.terms = pterms;
-        this.length = pterms.length;
     }
 
     removeTerm(term)
     {
-        for (var i = 0; i < this.length; i++)
-        {
-            if (this.terms[i].equal(term))
-            {
-                this.terms.splice(i,i);
-                this.length -= 1;
+        for (var i = 0; i < this.terms.length; i++) {
+            if (this.terms[i].equal(term)) {
+                var newTerms = [];
+                for (var j = 0; j < this.terms.length; j++) {
+                    if (j != i) {
+                        newTerms.push(this.terms[j]);
+                    }
+                }
+                this.terms = newTerms;
+                break;
             }
         }
     }
@@ -132,7 +154,6 @@ class lexp
         if (! this.containsTerm(term))
         {
             this.terms.push(term);
-            this.length += 1;
         }
     }
 
@@ -146,7 +167,7 @@ class lexp
     avgVar()
     {
         var varCounter = 0;
-        for (var i = 0; i < this.length; i++)
+        for (var i = 0; i < this.terms.length; i++)
         {
             varCounter += varCount - this.terms[i].countVal(-1);
         }
@@ -159,7 +180,7 @@ class lexp
 
     includesTerm(term)
     {
-        for (var i = 0; i < this.length; i++)
+        for (var i = 0; i < this.terms.length; i++)
         {
             if (this.terms[i].includes(term))
             {
@@ -171,7 +192,7 @@ class lexp
 
     findIncludedTerm(term)
     {
-        for (var i = 0; i < this.length; i++)
+        for (var i = 0; i < this.terms.length; i++)
         {
             if (term.includes(this.terms[i]))
             {
@@ -183,7 +204,7 @@ class lexp
 
     containsTerm(term)
     {
-        for (var i = 0; i < this.length; i ++)
+        for (var i = 0; i < this.terms.length; i++)
         {
             if (this.terms[i].equal(term))
             {
@@ -193,9 +214,58 @@ class lexp
         return false;
     }
 
+    addSymmetrie() {
+        var term = this.terms[randomInt(0, this.terms.length - 1)];
+        for (var j = 0; j < this.terms.length; j++) {
+            var differ = term.differAt(this.terms[j]);
+            if (differ.length == 2) {
+                var i = randomInt(0,1);
+                term.flip(differ[i]);
+                return;
+            }
+        }
+        for (var j = 0; j < this.terms.length; j++) {
+            var differ = term.differAt(this.terms[j]);
+            if (differ.length == 3) {
+                var i = randomInt(0,2);
+                term.flip(differ[i]);
+                return;
+            }
+        }
+        
+    }
+
+    removeSymmetrie(solution) {
+        var min = [new pterm([1,1,1])]
+        for (var i = 0; i < solution.terms.length; i++) {
+            if (solution.terms[i].countVal(-1) > min[0].countVal(-1)) {
+                min = [solution.terms[i]];
+            }
+            else if (solution.term[i].countVal(-1) == min[0].countVal(-1)) {
+                min = min + [solution.term[i]];
+            }
+        }
+        var j = randomInt(0, min.length - 1);
+        this.break(min[j]);
+    }
+
+    break(minTerm) {
+        var v = minTerm.getVarIndex();
+        var e = new lexp([minTerm]);
+        var le = e.genExpanded();
+        var i = randomInt(0, le.length);
+        var j = randomInt(0, v.length);
+        if (le.terms[i].term[j] == 0) {
+            this.changeTerm(le.terms[i], j, 1);
+        }
+        else {
+            this.changeTerm(le.terms[i], j, 0);
+        }
+    }
+
     notExpanded()
     {
-        for (var i = 0; i < this.length; i++)
+        for (var i = 0; i < this.terms.length; i++)
         {
             if (this.terms[i].countVal(-1) > 0)
             {
@@ -208,7 +278,7 @@ class lexp
     genExpanded()
     {
         var res = new lexp([]);
-        for (var i = 0; i < this.length; i++)
+        for (var i = 0; i < this.terms.length; i++)
         {
             if (this.terms[i].countVal(-1) > 0)
             {
@@ -231,7 +301,7 @@ class lexp
     toTableHTML()
     {
         var res = "";
-        for (var i = 0; i < this.length; i++)
+        for (var i = 0; i < this.terms.length; i++)
         {
             if (i % 2 == 1)
             {
@@ -262,11 +332,11 @@ class lexp
         }
         else
         {
-            for (var i = 0; i < this.length; i++)
+            for (var i = 0; i < this.terms.length; i++)
             {
                 var term = this.terms[i];
                 res += term.toHTML();
-                if (i != this.length - 1 || activeTerm.countVal(-1) != activeTerm.length)
+                if (i != this.terms.length - 1 || activeTerm.countVal(-1) != activeTerm.length)
                 {
                     res += " + ";
                 }
@@ -279,11 +349,11 @@ class lexp
 
     equal(lother)
     {
-        if (lother.length != this.length)
+        if (lother.terms.length != this.terms.length)
         {
             return false;
         }
-        for (var i = 0; i < this.length; i ++)
+        for (var i = 0; i < this.terms.length; i ++)
         {
             if (! this.containsTerm(lother.terms[i]))
             {
@@ -296,7 +366,7 @@ class lexp
     deepcopy()
     {
         var newExp = new lexp([])
-        for (var i = 0; i < this.length; i++)
+        for (var i = 0; i < this.terms.length; i++)
         {
             newExp.addTerm(this.terms[i].copy());
         }
@@ -409,6 +479,57 @@ function clearButtons()
     document.getElementById("buttons").innerHTML = ""
 }
 
+
+//////////////////
+//  MC CLUSKEY  //
+//////////////////
+
+function solve(canExp) {
+    var newIter = true;
+    while (newIter) {
+        var table = makeTable(canExp);
+        var newTerms = canExp.deepcopy();
+        for (var i = 0; i < varCount; i++) {
+            for (var j = 0; j < table[i].length; j++) {
+                for (var k = 0; k < table[i+1].length; k++) {
+                    var differ = table[i][j].differAt(table[i+1][k]);
+                    if (differ.length == 1) {
+                        var term12 = reduce(table[i][j], differ[0]);
+                        newTerms.removeTerm(table[i][j]);
+                        newTerms.removeTerm(table[i+1][k]);
+                        newTerms.addTerm(term12);
+                    }
+                }
+            }
+        }
+        if (newTerms.equal(canExp)) {
+            newIter = false;
+        }
+        else {
+            canExp = newTerms;
+        }
+    }
+    return canExp;
+}
+
+function makeTable(canExp) {
+    var res = {};
+    for (var i = 0; i <= varCount; i++) {
+        res[i] = [];
+    }
+    for (var i = 0; i < canExp.terms.length; i++) {
+        var counti = canExp.terms[i].countVal(1);
+        res[counti].push(canExp.terms[i]);
+    }
+    return res;
+}
+
+function reduce(term, i) {
+    var result = term.copy();
+    result.term[i] = -1;
+    return result;
+}
+
 //////////////////
 //  GENERATOR   //      Genereren van een oplossing die dan wordt ge-expand naar het volledig probleem
 //////////////////
@@ -416,10 +537,24 @@ function clearButtons()
 function generateBeginner()
 {
     varCount = 3;
-    var termCount = randomInt(2,3);
-    sol = generateRandom(termCount, 1.35, 1.75);
-    problem = sol.deepcopy();
-    problem = problem.genExpanded();
+    var termCount = randomInt(3,4);
+    var possibleTerms = new lexp([new pterm([1,1,1]), new pterm([1,1,0]), new pterm([1,0,1]), new pterm([1,0,0]), new pterm([0,1,1]), new pterm([0, 1, 0]), new pterm([0,0,1]), new pterm([0,0,0])]);
+    problem = new lexp([new pterm([1,0,0]), new pterm([0,1,0]), new pterm([0,0,1])]);
+    sol = solve(problem);
+    //var valid = false;
+    //while (! valid) {
+        //valid = true;
+        if (sol.terms.length == problem.terms.length) {
+            problem.addSymmetrie();
+            sol = solve(problem);
+        //    valid = false;
+        }
+        else if (sol.terms.length == 1) {
+            problem.removeSymmetrie(sol);
+            sol = solve(problem);
+            valid = false;
+        }
+    //}
     emptyAnswer();
     main();
 }
@@ -469,45 +604,30 @@ function generateExpert()
     main();
 }
 
-function generateTerm(exp, avgVarStart, avgVarEnd)
+function chooseTerm()
 {
-    var term = new pterm(Array(varCount).fill(-1))
-    for (var i = 0; i < varCount; i++)
-    {
-        term.setVar(i, randomInt(-1,1));
-    }
-    if (! between(exp.avgVar(), avgVarStart, avgVarEnd))
-    {
-        while (between(term.countVal(-1), varCount - avgVarEnd, varCount - avgVarStart))
-        {
-            term.setVar(randomInt(0,varCount - 1), -1);
+    var newTerm = [];
+    for (var i = 0; i < varCount; i++) {
+        var j = randomInt();
+        if (j > 0,5) {
+            newTerm += 1;
+        }
+        else {
+            newTerm += 0;
         }
     }
-    return term;
+    return new pterm(newTerm);
 }
 
-function generateRandom(terms, avgVarStart, avgVarEnd)
+function generateRandom(termCount, possibleTerms)
 {
-    res = new lexp([]);
-    for (var i = 0; i < terms;)
+    var res = new lexp([]);
+    for (var i = 0; i < termCount; i++)
     {
-        var term = generateTerm(res, avgVarStart, avgVarEnd);
-        if (! res.includesTerm(term) && res.findIncludedTerm(term) == undefined)
-        {
-            var minimalize = false;
-            for (var j = 0; j < res.length; j++)
-            {
-                if (res.terms[j].differAt(term).length == 1)
-                {
-                    minimalize = true;
-                }
-            }
-            if (! minimalize)
-            {
-                res.addTerm(term);
-                i++;
-            }
-        }
+        var j = randomInt(0, possibleTerms.terms.length - 1);
+        var term = possibleTerms.terms[j];
+        res.addTerm(term);
+        possibleTerms.removeTerm(term);
     }
     return res
 }
